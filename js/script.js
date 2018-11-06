c39e4fdf4aa6a0a4b09a777758f59c6e04d4e6c2
ko4an <eechor1mail.ru>
2018-11-06 12:50:14 +0300

    ver0.3

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ $(document).ready(function(){
 	var t=60;
 	var i=0;
 	$(".start").click(function(){
-        $(".ball1").css({"display":"block"})
+			$(".ball1").css({"display":"block"})
+			setInterval(function() {
+				$(".tstart").css({"margin":"5px"})
+				$(".tstart").html("Restart")
+           		$(".start").click(function(){
+            	location.reload();
+            	})
+           	},60000);  
 	});
     $(".ball1").click(function(){
     	$(".ball1").css({"display":"none"})
@@ -18,20 +25,19 @@ $(document).ready(function(){
     		    $(".ttimer").html("Time Out")	
     		}
     	},1000);
-    	
-    	});
+    });
     	$(".ball").click(function(){
     	var top=Math.random()*(446-0)+0;
     	var left=Math.random()*(446-0)+0;
-    	   i=i+1;
+    	    i=i+1;
     	setTimeout(function() {
     		$(".ttime").html(i)
     		setTimeout(function() {
-    			i=0;
-    		},10000)
+    			$(".res").css({"display":"block"})
+    		},1000)
             },60000);
     	$(".ball").css({"top":top});
     	$(".ball").css({"left":left})
     	return;
     });
-    });
+});
